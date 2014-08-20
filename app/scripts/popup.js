8710a3a2149132fba1c4a12dee2b7d1a12fa70c3
Margaret Godowns <margaretgodowns@gmail.com>
2014-08-19 23:34:25 -0400

    trying to get stuff on the page

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,8 +1,30 @@
-$.ajax({
-  url: 'http://www.reddit.com/r/listentothis/.json',
-  type: 'GET'
-})
-  .success(function(data){
-    console.log(data.data.children[1].data.url);
+$(document).ready(function(){
 
-})
+  $.ajax({
+    url: 'http://www.reddit.com/r/listentothis.json',
+    method: 'GET',
+
+    success:(function(data){
+
+      var songPosts = "<ul>"
+      for(var i = 0; i < 5; i++){
+
+        songPosts += '<li><a href="' + data.data.children[i].data.url + '"><img src ="' + data.data.children[i].data.thumbnail + ">" + data.data.children[i].data.title + '<li>';
+
+      }
+
+      songPosts += '</ul>';
+
+    $(".songLinks").html(songPosts);
+
+    console.log(data.data.children[i].data.thumbnail)
+  }
+
+)
+
+});
+
+
+
+
+});
